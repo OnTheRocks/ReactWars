d1e30563b3f8b1fa38b661ce4d0cb4224a046810
Nathan <nathan_huber@yahoo.com>
2021-09-09 01:11:38 -0500

    Pushing nightly progress.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Nathan Huber</h1>
+      <p>I'm the coolest dude around!  Below I've listed some of my favorite bands.</p>
+      <ul>
+        <li>AC/DC</li>
+        <li>Avengend Sevenfold</li>
+        <li>Papa Roach</li>
+      </ul>
+    </div>
+  )
+}
+
 // ReactDOM.render(What Do I Want To render, Where Do I Want To Render It);
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
-
-
+// ReactDOM.render(<div><h1>Hello World!</h1><p>What's up with that!??</p></div>, document.getElementById('root'));
+ReactDOM.render(
+  <MyApp />,
+  document.getElementById('root'));
