a802bfd4156cae5e62a00597fca9246ec82c2810
Nathan <nathan_huber@yahoo.com>
2021-09-12 23:57:37 -0500

    Created components folder and MyInfo.js.

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Nathan Huber</h1>
+      <p>I'm the coolest dude around!  Below I've listed some of my favorite bands.</p>
+      <ul>
+        <li>AC/DC</li>
+        <li>Avengend Sevenfold</li>
+        <li>Papa Roach</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyApp() {
-  return (
-    <div>
-      <h1>Nathan Huber</h1>
-      <p>I'm the coolest dude around!  Below I've listed some of my favorite bands.</p>
-      <ul>
-        <li>AC/DC</li>
-        <li>Avengend Sevenfold</li>
-        <li>Papa Roach</li>
-      </ul>
-    </div>
-  )
-}
+import MyInfo from "./components/MyInfo"
+
 
 // ReactDOM.render(What Do I Want To render, Where Do I Want To Render It);
 // ReactDOM.render(<div><h1>Hello World!</h1><p>What's up with that!??</p></div>, document.getElementById('root'));
 ReactDOM.render(
-  <MyApp />,
+  <MyInfo />,
   document.getElementById('root'));
