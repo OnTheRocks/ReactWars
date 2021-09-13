de1f6b71c3f83aec8e0168b1416d93bbcafb8259
Nathan <nathan_huber@yahoo.com>
2021-09-13 00:13:48 -0500

    Messing with Parent/Child Components.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import MyInfo from "./components/MyInfo"
+import App from "./App"
 
 
 // ReactDOM.render(What Do I Want To render, Where Do I Want To Render It);
 // ReactDOM.render(<div><h1>Hello World!</h1><p>What's up with that!??</p></div>, document.getElementById('root'));
 ReactDOM.render(
-  <MyInfo />,
+  <App />,
   document.getElementById('root'));
