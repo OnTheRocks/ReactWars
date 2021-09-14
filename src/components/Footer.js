391727b63e4f3b898408ae5d6b198ff25403b3d8
Nathan <nathan_huber@yahoo.com>
2021-09-14 01:36:57 -0500

    Added some inline styling.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Footer() {
+  const styles = {color: "green", backgroundColor: "black"};
   return (
     <footer>
-      <h3>This is my footer element</h3>
+      {/* <h3 style={{color: "green", backgroundColor: "black"}}>This is my footer element</h3> */}
+      <h3 style={styles}>This is my footer element</h3>
     </footer>
   )
 }
