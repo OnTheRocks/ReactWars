67ed063f58a2e6e04511490fbd88bdba19739536
Nathan <nathan_huber@yahoo.com>
2021-10-11 14:37:33 -0500

    Playing with state conditianal redering and state.

--- src/Temp.js
+++ src/Temp.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react'
 
+// export default class Apple extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       name: "Big Nate",
+//       age: "43"
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.name}</h1>
+//         <h3>{this.state.age} years old.</h3>
+//       </div>
+//     )
+//   }
+// }
+
+
+
 export default class Apple extends Component {
   constructor() {
     super()
     this.state = {
-      name: "Big Nate",
-      age: "43"
-    }
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.state.name}</h1>
-        <h3>{this.state.age} years old.</h3>
-      </div>
-    )
+      isLoggedIn: false
   }
 }
-
+  render( ){
+    let wordDisplay
+    if (this.state.isLoggedIn === true) {
+      wordDisplay = "in"
+    } else {
+      wordDisplay = "out"
+    }
+    return (
+      <div>
+          <h1>You are currently logged {wordDisplay}.</h1>
+          
+      </div>
+    ) 
+  }     
+}
