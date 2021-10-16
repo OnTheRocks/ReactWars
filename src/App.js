f8d5659a50fbf7b39e1ca2ad2fc727dd42cc2cd0
Nathan <nathan_huber@yahoo.com>
2021-10-16 01:14:05 -0500

    Working with State and Forms.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./components/Header"
 // import Navbar from "./components/Navbar"
-import MyInfo from "./components/MyInfo"
+// import MyInfo from "./components/MyInfo"
 // import Footer from "./components/Footer"
 import Apple from "./Temp"
 
@@ -11,7 +11,7 @@ function App() {
     <div>
       <Header />
       {/* <Navbar /> */}
-      <MyInfo />
+      {/* <MyInfo /> */}
       {/* <Footer /> */}
       <Apple />
     </div>
