a0de5b84eb473f7fb35dd137cf136a81e6988a66
Nathan <nathan_huber@yahoo.com>
2021-10-15 23:37:15 -0500

    More State & Forms.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
 import Header from "./components/Header"
-import Navbar from "./components/Navbar"
+// import Navbar from "./components/Navbar"
 import MyInfo from "./components/MyInfo"
-import Footer from "./components/Footer"
+// import Footer from "./components/Footer"
 import Apple from "./Temp"
 
 
--- src/Temp.js
+++ src/Temp.js
@@ -5,17 +5,18 @@ export default class Apple extends Component {
     super()
     this.state = {
       firstName: "",
-      lastNmae: ""
+      lastName: "",
+      isFriendly: true,
+      gender: "",
+      faveBand: ""
       
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
-    const {name, value} = event.target
-    this.setState({
-      [name]: value 
-    })
+    const {name, value, type, checked} = event.target
+    type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
   }
 
   render() {
@@ -29,10 +30,63 @@ export default class Apple extends Component {
           value={this.state.firstName} 
           onChange={this.handleChange} 
         />
-       <br />
+        <br />
         <input type="text" placeholder="Last Name" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
-        <h1>{this.state.firstName} {this.state.lastName}</h1>
-        <button type="submit">Submit</button>
+        <h1>{this.state.firstName} {this.state.lastName}</h1>        
+   
+        <textarea onChange={this.handleChange} value="Let's go Brandon!" />
+        <br />
+         <label>
+            <input 
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          /> Friendly?
+        </label>
+        <br />
+         <label>
+            <input 
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
+            onChange={this.handleChange}
+          /> Male
+        </label>
+        <label>
+            <input 
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          /> Female
+        </label>
+        <br />
+        <br />
+
+        <label>Favorite Band: </label>
+        <select 
+          onChange={this.handleChange}
+          name="favBand"
+          value={this.state.favBand}
+        >
+            <option value="AC/DC">AC/DC</option>
+            <option value="Avenged Sevenfold">Avenged Sevenfold</option>
+            <option value="Papa Roach">Papa Roach</option>
+            <option value="Volbeat">Volbeat</option>
+            <option value="Slipknot">Slipknot</option>
+            <option value="Pet Shop Boys">Pet Shop Boys</option>
+        </select>
+        <br />
+        <br />
+        <button>Submit</button>
+        <br />
+
+        <h2>You are a {this.state.gender}.</h2>
+        <h2>You're favorite band is:  {this.state.favBand}</h2>
+
       </form>
     )
   }
