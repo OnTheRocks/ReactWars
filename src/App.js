0744aece710decb270797b95d29984b2eb96de3f
Nathan <nathan_huber@yahoo.com>
2021-10-17 23:50:12 -0600

    moved temp.js into Temp folder.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 // import Navbar from "./components/Navbar"
 // import MyInfo from "./components/MyInfo"
 // import Footer from "./components/Footer"
-import Apple from "./Temp"
+import Apple from "./Temp/Temp"
 
 
 function App() {
